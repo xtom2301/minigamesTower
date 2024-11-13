d374849797e17da295d329d7253bba75bfb30b61
tmartin <xtom2301@gmail.com>
2024-11-13 20:25:02 +0100

    only show unscamble game when word is ready

--- src/pages/Unscramble.tsx
+++ src/pages/Unscramble.tsx
@@ -12,6 +12,7 @@ const Unscramble = () => {
   const [attempts, setAttempts] = useState(3);
   const [error, setError] = useState(false);
   const { setUnscrambleScore } = useUnscrambleHighscore();
+
   const getNewWord = async () => {
     const result = await fetchNewWord();
     if (result) {
@@ -55,45 +56,46 @@ const Unscramble = () => {
       </div>
     );
 
-  return (
-    <div>
-      <BackButton />
-      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[300px] sm:w-[600px] flex flex-col items-center">
-        <Score game="unscramble" />
-        <div className="text-2xl text-gray-600 dark:text-white">
-          Attempts left: {attempts}
-        </div>
-        <div className="text-6xl text-gray-600 my-10 dark:text-white">
-          {currentWord}
-        </div>
-        <input
-          className="w-3/4 p-4 rounded-md text-gray-600 text-center border-2 border-gray-600 focus:outline-none focus:border-gray-800 "
-          placeholder="Enter your answer"
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button
-          className="bg-gray-600 text-white p-4 rounded-md hover:scale-105 transition-all mt-6"
-          onClick={() => {
-            if (input.trim() !== "") {
-              checkAnswer();
-              setError(false);
-            } else {
-              setError(true);
-            }
-          }}
-        >
-          Submit
-        </button>
-        {error && (
-          <div className="text-red-500 text-xl mt-8">
-            Please enter an answer
+  if (currentWord)
+    return (
+      <div>
+        <BackButton />
+        <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[300px] sm:w-[600px] flex flex-col items-center">
+          <Score game="unscramble" />
+          <div className="text-2xl text-gray-600 dark:text-white">
+            Attempts left: {attempts}
           </div>
-        )}
+          <div className="text-6xl text-gray-600 my-10 dark:text-white">
+            {currentWord}
+          </div>
+          <input
+            className="w-3/4 p-4 rounded-md text-gray-600 text-center border-2 border-gray-600 focus:outline-none focus:border-gray-800 "
+            placeholder="Enter your answer"
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            className="bg-gray-600 text-white p-4 rounded-md hover:scale-105 transition-all mt-6"
+            onClick={() => {
+              if (input.trim() !== "") {
+                checkAnswer();
+                setError(false);
+              } else {
+                setError(true);
+              }
+            }}
+          >
+            Submit
+          </button>
+          {error && (
+            <div className="text-red-500 text-xl mt-8">
+              Please enter an answer
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Unscramble;
